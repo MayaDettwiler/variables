f9145919c3f8782e5f701c905c00afba80375459
MayaDettwiler <73507307+MayaDettwiler@users.noreply.github.com>
2020-10-26 21:34:46 -0500

    Initial commit

---
+++ variableDrills.py
@@ -0,0 +1,75 @@
+'''
+For this assignment you should read the task, then below the task do what it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a variable called name and assign it to your name. And print the variable.
+name = "Michael"
+print(name)
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+name = "Maya"
+'''INTEGERS'''
+var1 = 5
+print(var1)
+var2 = 10
+print(var2)
+var3 = var2
+print(var3)
+x = 24 
+print(x)
+y = 100
+print(y)
+
+'''FLOATS'''
+z = 1.1
+print(z)
+a = 2.75
+print(a)
+b = a
+print(b)
+c = 100.1 
+print(c)
+d = 12.3
+print(d)
+
+'''STRINGS'''
+name = "maya"
+print(name)
+e = "m"
+print(e)
+f = "a"
+print(f)
+g = "chicken"
+print(g)
+h = "may"
+print(h)
+
+'''BOOLEANS'''
+done = False
+print(done)
+smart = True
+print(smart)
+i = done
+print(i)
+j = False
+print(j)
+almostDone = True
+print(almostDone)
+
+'''LISTS'''
+listInt = [var1, var2, var3, x, y]
+print[var3]
+listStrings = [name, e, f, g, h]
+print[g]
+listBooleans = [done, smart, i, j, almostDone]
+print[done]
+print[smart]
+print[i]
+print[j]
+print[almostDone]
