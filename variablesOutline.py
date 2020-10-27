c4dca1e95026675edd26213fc3ab9c2fb5c86302
MayaDettwiler <73507307+MayaDettwiler@users.noreply.github.com>
2020-10-26 21:56:03 -0500

    Create variablesOutline.py

---
+++ variablesOutline.py
@@ -0,0 +1,58 @@
+'''
+This outline will help solidify concepts from the Variables lesson.
+Fill in this outline as the instructor goes through the lesson.
+'''
+
+#1)
+#Make an example variable of each of the types of variables (int, float, 
+#string, boolean and list):
+var1 = (int)
+var2 = 2
+var3 = 3.6
+var4 = "beans"
+listVar2 = [2]
+
+#2)
+#Make two variables one named 'a' and one named 'A' both set to  different data:
+#(Remember that capitalization matters, so these can be two different variables)
+a = 2
+A = 5
+
+#3)
+#Make a variable, and then go down a couple lines and change the data in that
+#same variable:
+var6 = 1
+
+
+
+var6 = 4
+
+#4)
+#Make a variable and set it to a variable you have already created:
+
+var7 = var1
+
+#5)
+#Make a list with three items. Make a variable that is set to the second
+#item in that list:
+listInt = [1, 2, 3]
+a = 2
+
+#6)
+#Make a String set to your name. Make a variable that is set to the first item
+#in that list:
+
+name = "maya"
+
+var30 = (maya)
+
+
+#7)
+#Print out ONE of any of the variables you have created so far:
+
+print(var3)
+
+#8)
+#Make a variable of any type, with one of the common SYNTAX errors
+
+Vari3 = 1,3,4
